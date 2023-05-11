f02d9ede01b64b425333d04c26a7d1a7ed172a6a
al3xkras <alexandr.krasovskiy@gmail.com>
2023-05-11 20:25:04 +0200

    smali_trace.py: implement trace injection

--- smali_trace.py
+++ smali_trace.py
@@ -88,34 +88,33 @@ for root,dir_,filenames in os.walk(directory):
             with open(filepath, "w") as file:
                 i = 0
 
-                def l(i_)->str:
+                def l(i_):
                     return lines[i_].strip()
 
+                def method_predicate(signature):
+                    return not "abstract" in signature \
+                        and not "native" in signature
+
                 while i < len(lines):
-                    if l(i).startswith(".class"):
-                        class_name=l(i).split()[1]
+                    l_i = l(i)
+                    if l_i.startswith(".class"):
+                        class_name=l_i.split()[1]
                         package_name = ".".join(class_name.split(".")[:-1])
                         file.write(lines[i])
                         i+=1
                         continue
 
-                    if l(i).startswith(".source"):
-                        file.write(l(i)+static_void_trace)
+                    if l_i.startswith(".source"):
+                        file.write(l_i+static_void_trace)
                         i+=1
                         continue
 
-                    if l(i).startswith(".method"):
-                        locals_count = 1
-                        while not l(i).startswith(".end method"):
-                            if l(i).startswith(".locals"):
-                                locals_count = int(l(i).split()[1])
-                                file.write(".locals {}\n".format(locals_count))
-                                method_name = l(i-1).split()[-1]
-                                file.write(string_var + "\n")
-                                file.write(f"    invoke-static {{}}, L{package_name};->traceLastMethodCall()V\n")
-                            else:
-                                file.write(lines[i])
-                            i += 1
-                    else:
-                        file.write(lines[i])
-                        i += 1
+                    if l_i.startswith(".locals") and method_predicate(l(i-1)):
+                        locals_count = int(l_i.split()[1])
+                        file.write(".locals {}\n".format(locals_count))
+                        file.write(f"    invoke-static {{}}, L{package_name};->traceLastMethodCall()V\n")
+                        i+=1
+                        continue
+
+                    file.write(lines[i])
+                    i += 1
