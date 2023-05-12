ebc1d0327f23474f5adedd9a5ac8f24a6baede9a
al3xkras <alexandr.krasovskiy@gmail.com>
2023-05-12 16:48:56 +0200

    smali_trace.py: implementation fixes (define .method public static traceLastMethodCall()V before the definition of the first class method)

---
+++ logcat_analysis.py
@@ -0,0 +1,10 @@
+def most_common_packages(logcat_file, n=10):
+    pass
+
+
+def most_common_classes(logcat_file, n=10):
+    pass
+
+
+def most_common_methods(logcat_file, n=10):
+    pass
--- smali_trace.py
+++ smali_trace.py
@@ -3,14 +3,23 @@ import os
 # directory path of the .smali files
 directory = "./"
 
-with open("./trace.smali","r") as f:
-    static_void_trace=f.read()
+ignored_packages = [
+    "google", "android",
+    "kotlin", "kotlinx",
+    "java", "javax",
+]
 
-for root,dir_,filenames in os.walk(directory):
+with open("./trace.smali", "r") as f:
+    static_void_trace = f.read()
+
+for root, dir_, filenames in os.walk(directory):
+    if any(x in root for x in ignored_packages):
+        print("package ignored:", root)
+        continue
     for filename_ in filenames:
-        filename=os.path.join(root, filename_)
+        filename = os.path.join(root, filename_)
         print(filename)
-        package_name=""
+        package_name = ""
         if filename.endswith(".smali"):
             filepath = os.path.join(directory, filename)
 
@@ -20,34 +29,38 @@ for root,dir_,filenames in os.walk(directory):
             with open(filepath, "w") as file:
                 i = 0
 
-                def l(i_):
+                def strip_line(i_):
                     return lines[i_].strip()
 
                 def method_predicate(signature):
                     return not "abstract" in signature \
                         and not "native" in signature
 
+                class_name = None
+                trace_added = False
                 while i < len(lines):
-                    l_i = l(i)
+                    l_i = strip_line(i)
                     if l_i.startswith(".class"):
-                        class_name=l_i.split()[-1][1:-1]
+                        class_name = l_i.split()[-1][1:-1]
                         package_name = ".".join(class_name.split(".")[:-1])
-                        print("CLASS: ",class_name,package_name)
+                        print("CLASS: ", class_name, package_name)
 
                         file.write(lines[i])
-                        i+=1
+                        i += 1
                         continue
 
-                    if l_i.startswith(".source"):
-                        file.write(l_i+static_void_trace)
-                        i+=1
+                    if l_i.startswith(".method") and not trace_added:
+                        file.write(static_void_trace + "\n"+l_i)
+                        i += 1
+                        trace_added=True
                         continue
 
-                    if l_i.startswith(".locals") and method_predicate(l(i-1)):
+                    if l_i.startswith(".locals") and method_predicate(strip_line(i - 1)):
                         locals_count = int(l_i.split()[1])
                         file.write(".locals {}\n".format(locals_count))
-                        file.write(f"    invoke-static {{}}, L{class_name};->traceLastMethodCall()V\n")
-                        i+=1
+                        if class_name is not None:
+                            file.write(f"    invoke-static {{}}, L{class_name};->traceLastMethodCall()V\n")
+                        i += 1
                         continue
 
                     file.write(lines[i])
